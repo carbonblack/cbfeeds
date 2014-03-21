df5037eed0e32c5236fd2749c04317e7f0f46cbb
Scott Lundgren <scott.lundgrne@carbonblack.com>
2014-03-21 13:41:54 -0700

    - added validation feed

---
+++ validate_feed.py
@@ -0,0 +1,93 @@
+import re
+import sys
+import time
+import urllib
+import json
+import optparse
+import socket
+import base64
+import cbfeeds
+
+def _build_cli_parser():
+    usage = "usage: %prog [options]"
+    desc = "Convert a flat file of IOCs to a Carbon Black feed"
+
+    parser = optparse.OptionParser(usage=usage, description=desc)
+    
+    parser.add_option("-f", "--feedfile", action="store", type="string", dest="feed_filename",
+                      help="Feed Filename to validate")
+
+    return parser
+
+def validate_file(feed_filename):
+    """
+    validate that the file exists and is readable
+    """
+    f = open(feed_filename)
+    contents = f.read()
+    return contents
+
+def validate_json(contents):
+    """
+    validate that the file is well-formed JSON
+    """
+    return json.loads(contents)
+
+def validate_feed(feed):
+    """
+    validate that the file is valid as compared to the CB feeds schema
+    """
+     
+    # verify that we have both of the required feedinfo and reports elements
+    #
+    if not feed.has_key("feedinfo"):
+        raise Exception("No 'feedinfo' element found!")
+    if not feed.has_key("reports"):
+        raise Exception("No 'reports' element found!")
+
+    # set up the cbfeed object
+    #
+    feed = cbfeeds.CbFeed(feed["feedinfo"], feed["reports"])
+
+    # serialize the feed
+    # this actually does the feed validation
+    #
+    feed.dump() 
+
+if __name__ == "__main__":
+
+    parser = _build_cli_parser()
+    options, args = parser.parse_args(sys.argv)
+
+    if not options.feed_filename:
+        print "-> Must specify a feed filename to validate"
+        sys.exit(0)
+
+    try:
+        contents = validate_file(options.feed_filename)
+        print "-> Validated that file exists and is readable"
+    except Exception, e:
+        print "-> Unable to validate that file exists and is readable"
+        print "-> Details:"
+        print
+        print e
+
+    try:
+        feed = validate_json(contents)
+        print "-> Validated that feed file is valid JSON"
+    except Exception, e:
+        print "-> Unable to validate that file is valid JSON"
+        print "-> Details:"
+        print
+        print e
+
+    try:
+        validate_feed(feed)
+        print "-> Validated that the feed file includes all necessary CB elements"
+        print "-> Validated that all element values are within CB feed requirements"
+        print
+    except Exception, e:
+        print "-> Unable to validate that the file is a valid CB feed"
+        print "-> Details:"
+        print
+        print e
