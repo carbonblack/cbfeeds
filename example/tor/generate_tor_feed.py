40610dffa653ce4648fd3d1d9000491b93449bc2
Administrator <admin@local.host>
2014-10-01 21:46:20 -0400

    * update regex to include .(dot) in Id field * Add relative path to support imports * Minor edits and reformatting * updated .gitignore to ignore pycharm IDE files and test feeds

--- example/tor/generate_tor_feed.py
+++ example/tor/generate_tor_feed.py
@@ -3,17 +3,15 @@ import sys
 import time
 import urllib
 import json
-from HTMLParser import HTMLParser
-from datetime import timedelta
 
 # third part lib imports
-import requests
 
 # our imports
 sys.path.insert(0, "../../")
 from cbfeeds import CbReport
 from cbfeeds import CbFeed
 from cbfeeds import CbFeedInfo
+
 
 def get_tor_nodes():
     nodes = []
@@ -28,8 +26,8 @@ def get_tor_nodes():
                 # process only IPv4 addresses for now
                 if address.count(':') == 1:
                     # All IPv4 addresses will end up here.
-                    ipv4,port = address.split(':')
-                    nodes.append({'ip': ipv4, 
+                    ipv4, port = address.split(':')
+                    nodes.append({'ip': ipv4,
                                   'name': entry['nickname'],
                                   'port': port,
                                   'firstseen': entry['first_seen'],
@@ -39,38 +37,41 @@ def get_tor_nodes():
             print "%s while parsing: %s" % (err, entry)
     return nodes
 
-def build_reports(nodes): 
+
+def build_reports(nodes):
     # TODO - this is one "report" per TOR node IP.  Not ideal.
     reports = []
     for node in nodes:
         fields = {'iocs': {
-                              'ipv4': [node['ip'], ]
-                           },
-                   'score': 0,
-                   'timestamp': int(time.mktime(time.gmtime())),
-                   'link': 'http://www.torproject.org',
-                   'id': "TOR-Node-%s" % node['ip'],
-                   'title':"%s has been a TOR exit node since %s and was last seen %s on port %s. Contact: %s" 
-                   % (node['ip'],node['firstseen'], node['lastseen'], node['port'], node['contact']) }
+            'ipv4': [node['ip'], ]
+        },
+                  'score': 0,
+                  'timestamp': int(time.mktime(time.gmtime())),
+                  'link': 'http://www.torproject.org',
+                  'id': "TOR-Node-%s" % node['ip'],
+                  'title': "%s has been a TOR exit node since %s and was last seen %s on port %s. Contact: %s"
+                           % (node['ip'], node['firstseen'], node['lastseen'], node['port'], node['contact'])}
         reports.append(CbReport(**fields))
 
     return reports
 
+
 def create():
     nodes = get_tor_nodes()
     reports = build_reports(nodes)
-    
+
     feedinfo = {'name': 'tor',
                 'display_name': "Tor Exit Nodes",
                 'provider_url': 'https://www.torproject.org/',
                 'summary': "This feed is a list of Tor Node IP addresses, updated every 30 minutes.",
                 'tech_data': "There are no requirements to share any data to receive this feed.",
                 'icon': 'images/tor.png'}
-            
+
     feedinfo = CbFeedInfo(**feedinfo)
     feed = CbFeed(feedinfo, reports)
 
     return feed.dump()
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
