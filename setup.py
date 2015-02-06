f27340633313e8f69dc7a284ba925ceb12070b7d
jjguy <jjg@jjguy.com>
2015-02-06 12:49:51 -0600

    updating version number to reflect example updates

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import sys
 
 setup(
     name='python-cbfeeds',
-    version='0.6',
+    version='0.6.1',
     url='http://github.com/carbonblack/cbfeeds',
     license='',
     author='Carbon Black',
