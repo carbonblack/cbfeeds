254ea76b5485697480ba1bd6897840a4feaf8aa6
Scott Lundgren <scott.lundgrne@carbonblack.com>
2015-03-19 06:14:30 -0700

    CBAPI-7 - add support for automatically prepending cb.urlver=1&q= to queries in percent encoder script

--- percent_encode_query.py
+++ percent_encode_query.py
@@ -14,8 +14,21 @@ def build_cli_parser():
 
     cmd_parser.add_option("-q", "--query", action="store", type="string", dest="query",
                           help="Query to encode")
-
+    cmd_parser.add_option("-n", "--no-prepend", action="store_false", default=True, dest="prepend",
+                          help="Do NOT prepend \"q=\" and \"cb.urlver=1\" when not found in the query specified with \"--query\"") 
     return cmd_parser
+
+def is_query_complete(query):
+    """
+    returns indication as to if query includes a q=, cb.q=, or cb.fq
+    """
+    if query.startswith("cb.urlver="):
+        return True
+    if query.startswith("q=") or \
+       query.startswith("cb.q=") or \
+       query.startswith("cb.fq="):
+        return True
+    return False
 
 if __name__ == "__main__":
 
@@ -25,7 +38,17 @@ if __name__ == "__main__":
     if not options.query:
         print "-> Must specify a query to encode; use the -q switch or --help for usage"
         sys.exit(0)
-    
+ 
     print options.query
     print
-    print urllib.quote(options.query)
+
+    # unless overridden by operator, prepend a cb.urlver=1&q= to the query if 
+    # if does not already exist.  this makes it possible for customer to copy and
+    # paste query from CB UI, pass through this script, and add to a feed
+    #
+    # see CBAPI-7
+    #
+    if options.prepend and not is_query_complete(options.query):
+        print "cb.urlver=1&q=" + urllib.quote(options.query)  
+    else:
+        print urllib.quote(options.query)
