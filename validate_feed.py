7f8193f6868f435781b2186d577cd84f418b8e6b
Filipe Spencer Lopes <filipe.spencerlopes@be.ibm.com>
2020-01-09 17:17:00 +0100

    Replaced optparse with argparse, can now process multiple feeds.

--- validate_feed.py
+++ validate_feed.py
@@ -1,7 +1,7 @@
+#!/usr/bin/env python
 import sys
 import json
-import optparse
-
+import argparse
 import cbfeeds
 
 
@@ -10,19 +10,34 @@ def build_cli_parser():
     generate OptionParser to handle command line switches
     """
 
-    usage = "usage: %prog [options]"
     desc = "Validate a Carbon Black feed"
 
-    cmd_parser = optparse.OptionParser(usage=usage, description=desc)
+    cmd_parser = argparse.ArgumentParser(description=desc)
 
-    cmd_parser.add_option("-f", "--feedfile", action="store", type="string", dest="feed_filename",
-                          help="Feed Filename to validate")
-    cmd_parser.add_option("-p", "--pedantic", action="store_true", default=False, dest="pedantic",
-                          help="Validates that no non-standard JSON elements exist")
-    cmd_parser.add_option("-e", "--exclude", action="store", default=None, dest="exclude",
-                          help="Filename of 'exclude' list - newline delimited indicators to consider invalid")
-    cmd_parser.add_option("-i", "--include", action="store", default=None, dest="include",
-                          help="Filename of 'include' list - newline delimited indicators to consider valid")
+    cmd_parser.add_argument(
+        "-f",
+        "--feed_filename",
+        type=str,
+        help="Feed Filename to validate",
+        required=True,
+        nargs='+')
+    cmd_parser.add_argument(
+        "-p",
+        "--pedantic",
+        action="store_true",
+        default=False,
+        help="Validates that no non-standard JSON elements exist")
+    cmd_parser.add_argument(
+        "-e",
+        "--exclude",
+        default=None,
+        help="Filename of 'exclude' list - newline delimited indicators to consider invalid")
+
+    cmd_parser.add_argument(
+        "-i",
+        "--include",
+        default=None,
+        help="Filename of 'include' list - newline delimited indicators to consider valid")
 
     return cmd_parser
 
@@ -50,13 +65,13 @@ def validate_feed(feed, pedantic=False):
 
     # verify that we have both of the required feedinfo and reports elements
     #
-    if not feed.has_key("feedinfo"):
+    if "feedinfo" not in feed:
         raise Exception("No 'feedinfo' element found!")
-    if not feed.has_key("reports"):
+    if "reports" not in feed:
         raise Exception("No 'reports' element found!")
 
+
     # set up the cbfeed object
-    #
     feed = cbfeeds.CbFeed(feed["feedinfo"], feed["reports"])
 
     # validate the feed
@@ -66,6 +81,7 @@ def validate_feed(feed, pedantic=False):
     feed.validate(pedantic)
 
     return feed
+
 
 def validate_against_include_exclude(feed, include, exclude):
     """
@@ -94,57 +110,64 @@ def gen_include_exclude_sets(include_filename, exclude_filename):
     return include, exclude
 
 
-if __name__ == "__main__":
-
-    parser = build_cli_parser()
-    options, args = parser.parse_args(sys.argv)
-
-    if not options.feed_filename:
-        print "-> Must specify a feed filename to validate; use the -f switch or --help for usage"
-        sys.exit(0)
-
+def validation_cycle(feed_filename):
     # generate include and exclude (whitelist and blacklist) sets of indicators
     # feed validation will fail if a feed ioc is blacklisted unless it is also whitelisted
     #
     include, exclude = gen_include_exclude_sets(options.include, options.exclude)
-
     try:
-        contents = validate_file(options.feed_filename)
-        print "-> Validated that file exists and is readable"
-    except Exception, e:
-        print "-> Unable to validate that file exists and is readable"
-        print "-> Details:"
-        print
-        print e
+        print('#> Testing feed {feed_file}'.format(feed_file=feed_filename))
+        contents = validate_file(feed_filename)
+        print("-> Validated that file exists and is readable")
+    except Exception as e:
+        print("-> Unable to validate that file exists and is readable")
+        print("-> Details:")
+        print()
+        print(e)
         sys.exit(0)
 
     try:
         feed = validate_json(contents)
-        print "-> Validated that feed file is valid JSON"
-    except Exception, e:
-        print "-> Unable to validate that file is valid JSON"
-        print "-> Details:"
-        print
-        print e
+        print("-> Validated that feed file is valid JSON")
+    except Exception as e:
+        print("-> Unable to validate that file is valid JSON")
+        print("-> Details:")
+        print()
+        print(e)
         sys.exit(0)
 
     try:
         feed = validate_feed(feed, pedantic=options.pedantic)
-        print "-> Validated that the feed file includes all necessary CB elements"
-        print "-> Validated that all element values are within CB feed requirements"
+        print("-> Validated that the feed file includes all necessary CB elements")
+        print("-> Validated that all element values are within CB feed requirements")
         if options.pedantic:
-            print "-> Validated that the feed includes no non-CB elements"
-    except Exception, e:
-        print "-> Unable to validate that the file is a valid CB feed"
-        print "-> Details:"
-        print
-        print e
+            print("-> Validated that the feed includes no non-CB elements")
+    except Exception as e:
+        print("-> Unable to validate that the file is a valid CB feed")
+        print("-> Details:")
+        print()
+        print(e)
         sys.exit(0)
 
     if len(exclude) > 0 or len(include) > 0:
         try:
             validate_against_include_exclude(feed, include, exclude)
-            print "-> Validated against include and exclude lists"
-        except Exception, e:
-            print "-> Unable to validate against the include and exclude lists"
-            print e 
+            print("-> Validated against include and exclude lists")
+        except Exception as e:
+            print("-> Unable to validate against the include and exclude lists")
+            print(e)
+
+
+if __name__ == "__main__":
+
+    parser = build_cli_parser()
+    options = parser.parse_args()
+    feed_filenames = options.feed_filename
+
+    if not feed_filenames:
+        print("-> Must specify a feed filename to validate; use the -f switch or --help for usage")
+        sys.exit(0)
+
+    for feed_filename in feed_filenames:
+        validation_cycle(feed_filename)
+        print('\n ----- \n')
